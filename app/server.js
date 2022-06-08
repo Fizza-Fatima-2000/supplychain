230a1241179c6f580df09e703877f5550b7cd8ea
Fizza-Fatima-2000 <fatimafizza96@gmail.com>
2022-06-08 23:58:06 +0500

    hello

--- app/server.js
+++ app/server.js
@@ -4,13 +4,14 @@ const bodyparser = require('body-parser');
 PORT = process.env.PORT || 7007
 require('./config/connectDB');
 require('dotenv').config()
-
+const cors = require('cors');
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 const user_router = require('./routes/users.routes');
 app.use(user_router)
 
-
+app.use(cors({
+    origin: 'http://localhost:3000'}));
 
 app.listen(PORT, () => {
     console.log(`server is start ${PORT}`)
