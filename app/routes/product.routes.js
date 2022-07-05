a2c6cfac9f9f4bfd14b2c9c5bbbe55255c6a85ce
Fizza-Fatima-2000 <fatimafizza96@gmail.com>
2022-07-06 01:19:38 +0500

    hello

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const product_router = express.Router();
-const { add_product,info_product} = require('../controllers/product.controller')
+const { add_product,info_product} = require('../controllers/product.controller');
+const verifyAdmin = require("../middleware/admin.auth");
+const verifyToken = require("../middleware/auth");
 
 
 
-product_router.post('/api/add_product',add_product)
-product_router.get('/api/get_product', info_product)
+product_router.post('/api/add_product',[verifyToken,verifyAdmin],add_product)
+product_router.get('/api/get_product',[verifyToken,verifyAdmin], info_product)
 
 
 
