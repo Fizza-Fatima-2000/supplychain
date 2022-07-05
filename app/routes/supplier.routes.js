45b6d6ed10c6b73ad568d8a03e3fd6067366debe
Fizza-Fatima-2000 <fatimafizza96@gmail.com>
2022-07-06 01:14:18 +0500

    hello

--- app/routes/supplier.routes.js
+++ app/routes/supplier.routes.js
@@ -2,13 +2,10 @@ const express = require("express");
 const supplier_router = express.Router();
 const { add_suplier,info_supplier} = require('../controllers/supplier.controller')
 
-const  verifyToken = require('../middleware/auth')
-
-const verifyAdmin = require("../middleware/admin.auth");
 
 
 
-supplier_router.post('/api/add_supplier' ,[verifyAdmin,verifyToken], add_suplier);
+supplier_router.post('/api/add_supplier' , add_suplier);
 supplier_router.get('/api/info_supplier' , info_supplier);
 
 
